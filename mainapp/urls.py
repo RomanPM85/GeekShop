a716b51a2f0ac45b9d9f9bcaac448a477b718c54
Roman <mamchiy@yandex.ru>
2021-01-30 01:50:14 +0300

    adjusting the namespace in the file .html, mainapp/urls.py

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from mainapp.views import products
 
+app_name = 'mainapp'
+
 urlpatterns = [
     path('', products, name='index'),
     path('<int:id>/', products, name='product'),
