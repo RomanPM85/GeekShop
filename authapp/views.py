2dc420dcbdb38634114e8559ab374a9c7e32e4b5
Roman <mamchiy@yandex.ru>
2021-02-01 02:22:07 +0300

    correct, user profile in display

--- authapp/views.py
+++ authapp/views.py
@@ -31,3 +31,7 @@ def register(request):
         form = UserRegisterForm()
     context = {'form': form}
     return render(request, 'authapp/register.html', context)
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
