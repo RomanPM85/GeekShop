57771229e03bcdefec04b079a33abd705fa9848b
Roman <mamchiy@yandex.ru>
2021-02-03 02:19:22 +0300

    adding information to the controller of the file views.py and file profile adjustment profile.html

--- authapp/forms.py
+++ authapp/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 
 from authapp.models import User
 
