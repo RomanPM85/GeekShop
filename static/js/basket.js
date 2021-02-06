84f571afd420e275124ca45eab7d06c52a836b3f
Roman <mamchiy@yandex.ru>
2021-02-06 23:05:23 +0300

    correction of html code in files /authapp/profile.html; /basket/basket.html; /mainapp/base.html and static/js/basket.js

--- static/js/basket.js
+++ static/js/basket.js
@@ -1,3 +1,7 @@
 window.onload = function () {
-    
+    $('.basket_list').on('click', 'input[type="number"]', function () {
+        var t_href = event.target;
+        console.log(t_href.name) // id корзины
+        console.log(t_href.value) // количество в корзине
+    });
 }
