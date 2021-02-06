d012ff5a679feda1b12f992e780d71be84ba717e
Roman <mamchiy@yandex.ru>
2021-02-06 23:22:53 +0300

    /basket.js augment code, ajax url, success, and event

--- static/js/basket.js
+++ static/js/basket.js
@@ -3,5 +3,13 @@ window.onload = function () {
         var t_href = event.target;
         console.log(t_href.name) // id корзины
         console.log(t_href.value) // количество в корзине
+        $.ajax({
+            url: "/baskets/edit/" + t_href.name + "/" + t_href.value + "/",
+            success: function (data) {
+                $('.basket_list').html(data.result);
+
+            }
+        });
+        event.preventDefault();
     });
 }
