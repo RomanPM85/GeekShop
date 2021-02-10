522c855e372674f52d530abc53bf64de4b9eb909
Roman <mamchiy@yandex.ru>
2021-02-11 00:27:55 +0300

    update it adminapp  admin-users-update

--- adminapp/forms.py
+++ adminapp/forms.py
@@ -13,3 +13,10 @@ class UserAdminRegisterForm(UserRegisterForm):
     def __init__(self, *args, **kwargs):
         super(UserAdminRegisterForm, self).__init__(*args, **kwargs)
         self.fields['avatar'].widget.attrs['class'] = 'custom-file-input'
+
+class UserAdminProfileForm(UserProfileForm):
+
+    def __init__(self, *args, **kwargs):
+        super(UserAdminProfileForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs['readonly'] = False
+        self.fields['email'].widget.attrs['readonly'] = False
--- adminapp/views.py
+++ adminapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
 
 from authapp.models import User
-from adminapp.forms import UserAdminRegisterForm
+from adminapp.forms import UserAdminRegisterForm, UserAdminProfileForm
 
 def index(request):
     return render(request, 'adminapp/index.html')
@@ -25,5 +25,13 @@ def admin_users_create(request):
     return render(request, 'adminapp/admin-users-create.html', context)
 
 def admin_users_update(request, id=None):
-
-    return render(request, 'adminapp/admin-users-update-delete.html')
+    user = User.objects.get(id=id)
+    if request.method == 'POST':
+        form = UserAdminProfileForm(data=request.POST, files=request.FILES, instance=user)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admins:admin_users_read'))
+    else:
+        form = UserAdminProfileForm(instance=user)
+    context = {'form': form, 'current_user': user}
+    return render(request, 'adminapp/admin-users-update-delete.html', context)
