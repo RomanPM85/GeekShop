5dbced8cc15987fb2d2d04cdc163161beacc112a
Roman <mamchiy@yandex.ru>
2021-02-13 15:47:10 +0300

    filtering with paginated output, importing paginator, controller description, catching try, except errors, creating pagination.

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,15 +1,35 @@
 from django.shortcuts import render
 
 from mainapp.models import Product, ProductCategory
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def index(request):
     return render(request, 'mainapp/index.html')
 
 
-def products(request, category_id=None):
-    context = {
-        'categories': ProductCategory.objects.all(),
-        'products': Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
-    }
+# def products(request, category_id=None):
+#     context = {
+#         'categories': ProductCategory.objects.all(),
+#         'products': Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
+#     }
+#     return render(request, 'mainapp/products.html', context)
+
+def products(request, category_id=None, page=1):
+    if category_id:
+        products = Product.objects.filter(category_id=category_id)
+    else:
+        products = Product.objects.all()
+    per_page = 3
+    paginator = Paginator(products, per_page)
+    # если ошибок нет
+    products_paginator = paginator.page(page)
+    context = {'categories': ProductCategory.objects.all(), 'products': products_paginator}
     return render(request, 'mainapp/products.html', context)
+    # вариант который рекомендует django, через try и except
+    # try:
+    #     products_paginator = paginator.page(page)
+    # except PageNotAnInteger:
+    #     products_paginator = paginator.page(1)
+    # except EmptyPage:
+    #     products_paginator = paginator.page(paginator.num_pages)
